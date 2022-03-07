204638137a90e29733a4c4cc23c01afcd5e273ab
Suraj <suragek24@gmail.com>
2022-03-07 19:11:54 +1300

    Initial swift app set up

---
+++ Shared/scalApp.swift
@@ -0,0 +1,17 @@
+//
+//  scalApp.swift
+//  Shared
+//
+//  Created by Suraj Kumar on 7/03/22.
+//
+
+import SwiftUI
+
+@main
+struct scalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

f78f5b6acbb8b0c43fcbe60c01a3ca2fcb154fc3
Suraj Kumar <suragek24@gmail.com>
2022-04-03 18:13:29 +1200

    Initial Skeleton

--- scal/scal/ViewController.swift
+++ scal/scal/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .black
         // Do any additional setup after loading the view.
     }
 

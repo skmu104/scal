455b2dc82f55b8d2e1fd88fdabd6cb66aa641e35
Suraj Kumar <suragek24@gmail.com>
2022-04-10 20:50:33 +1200

    Basic UI main screen with date template

---
+++ scal/scal/CustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCell.swift
+//  scal
+//
+//  Created by Suraj Kumar on 10/04/22.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTACDayCell {
+    @IBOutlet var dateLabel: UILabel!
+}
--- scal/scal/ViewController.swift
+++ scal/scal/ViewController.swift
@@ -6,16 +6,62 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class ViewController: UIViewController {
-
+    
+    //let formatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .black
+//        view.backgroundColor = .black
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
 
+//extension ViewController: JTACMonthViewDelegate,JTACMonthViewDataSource{
+//
+//
+//
+////    func calendar(_ calendar: JTACMonthView, willDisplay cell: JTACDayCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+////        let myCustomCell = cell as! CellView
+////
+////    }
+//
+//    func configureCalendar(_ calendar: JTACMonthView) -> ConfigurationParameters {
+//        formatter.dateFormat = "yyyy MM dd"
+//        formatter.timeZone = Calendar.current.timeZone
+//        formatter.locale = Calendar.current.locale
+//
+//        let startDate = formatter.date(from: "2022 01 01")!
+//        let endDate = formatter.date(from: "2022 12 31")!
+//        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+//
+//        return parameters
+//    }
+//
+//    func calendar(_ calendar: JTACMonthView, willDisplay cell: JTACDayCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+//        let myCustomCell = cell as! CustomCell
+//        sharedFunctionToConfigureCell(myCustomCell: myCustomCell, cellState: cellState, date: date)
+//    }
+//
+//    func calendar(_ calendar: JTACMonthView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTACDayCell{
+//        let myCustomCell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+//        sharedFunctionToConfigureCell(myCustomCell: myCustomCell, cellState: cellState, date: date)
+//        return myCustomCell
+//    }
+//
+//
+//    func sharedFunctionToConfigureCell(myCustomCell: CustomCell, cellState: CellState, date: Date) {
+//            myCustomCell.dateLabel.text = cellState.text
+//
+//            // more code configurations
+//            // ...
+//            // ...
+//            // ...
+//    }
+//}
+    
